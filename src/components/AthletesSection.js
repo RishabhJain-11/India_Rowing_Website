127dde0f467fa5e4ef717cc98ed0b726bf0759c8
Rishabh Jain <jainrishabh71011@gmail.com>
2023-02-28 23:27:08 +0530

    28-02-2023

--- src/components/AthletesSection.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AthletesSection = () => {
-  return (
-    <div>AthletesSection</div>
-  )
-}
-
-export default AthletesSection
--- src/components/NewsSection.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NewsSection = () => {
-  return (
-    <div>NewsSection</div>
-  )
-}
-
-export default NewsSection
