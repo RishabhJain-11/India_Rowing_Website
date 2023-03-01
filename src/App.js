612c473d8e6022107e641b15782cf4fb48519b84
Rishabh Jain <jainrishabh71011@gmail.com>
2023-03-01 23:46:08 +0530

    01-03-2023

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <nav>
+      <ul className='flex bg-gray-50 p-6 px-10 gap-5 font-bold text-green-400 border border-green-400'>
+        <li>Problems</li>
+        <li>Courses</li>
+        <li>Job Fair</li>
+        <li>POTD</li>
+        <li>Contest</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default App;
