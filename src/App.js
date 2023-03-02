ee54153395b63b67253de06ccb883bdfee91672a
Rishabh Jain <jainrishabh71011@gmail.com>
2023-03-02 11:40:29 +0530

    02-03-2023

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <nav>
-      <ul className='flex bg-gray-50 p-6 px-10 gap-5 font-bold text-green-400 border border-green-400'>
-        <li>Problems</li>
-        <li>Courses</li>
-        <li>Job Fair</li>
-        <li>POTD</li>
-        <li>Contest</li>
-      </ul>
-    </nav>
+    <>
+      <Navbar />
+    </>
   );
 }
 
