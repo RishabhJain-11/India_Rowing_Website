8e8608803ea79f01fd9bda2bbd2b9afb224202ac
Rishabh Jain <jainrishabh71011@gmail.com>
2023-03-02 12:36:58 +0530

    02-03-2023

---
+++ src/components/Hero.js
@@ -0,0 +1,29 @@
+export default function Hero() {
+    return (
+        <>
+            <div className="bg-gray-100 pb-12 overflow-y-hidden" style={{ minHeight: 700 }}>
+                <dh-component>
+                    <div className="bg-gray-50">
+                        <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
+                            <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col mb-5 sm:mb-10">
+                                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-gray-800 font-black leading-7 md:leading-10 ">
+                                    The Freedom to Create the
+                                    <span className="text-indigo-700">Websites</span>
+                                    You Want
+                                </h1>
+                                <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">
+                                    A professional website drives sales. Create a beautiful website to impress and engage new customers and establish your business online
+                                </p>
+                            </div>
+                            <div className="flex justify-center items-center">
+                                <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-indigo-700 transition duration-150 ease-in-out hover:bg-indigo-600 lg:font-bold rounded text-white px-4 sm:px-10 border border-indigo-700 py-2 sm:py-4 text-sm">Get Started</button>
+                                <button className="ml-4 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 bg-transparent transition duration-150 ease-in-out hover:border-indigo-600 lg:text-xl lg:font-bold  hover:text-indigo-600 rounded border border-indigo-700 text-indigo-700 px-4 sm:px-10 py-2 sm:py-4 text-sm">Live Demo</button>
+                            </div>
+                        </div>
+                    </div>
+                </dh-component>
+            </div>
+        </>
+    )
+    
+}
